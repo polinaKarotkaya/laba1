54e802830b331f9b88587c052d947d0298e51f25
Polina Karotkya <apollinariyad09@gmail.com>
2024-09-26 01:53:16 +0300

    Добавление операции деления и её использование в уравнении фрактала

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -86,6 +86,22 @@ public class Complex {
     }
 
     /**
+     * Division operation.
+     * @param b divisor
+     * @return this Complex object whose value is (this / b)
+     */
+    public Complex divide(Complex b) {
+        Complex a = this;
+        double denominator = b.re * b.re + b.im * b.im;
+        double real = (a.re * b.re + a.im * b.im) / denominator;
+        double imag = (a.im * b.re - a.re * b.im) / denominator;
+        re = real;
+        im = imag;
+        return this;
+    }
+
+
+    /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
      * @return square of length
